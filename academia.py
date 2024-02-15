0c6e8eb43eb48a1c4fecd6f0d2a24c38b09f64d7
wagber <wagber.dev@gmail.com>
2024-02-14 23:49:21 -0300

    Novas adições de código no arquivo academia.py

--- academia.py
+++ academia.py
@@ -1,4 +1,5 @@
 import random
+#import seaborn as sns
 
 class Academia:
     def __init__ (self):
@@ -11,6 +12,9 @@ class Academia:
 
     def listar_halteres(self):
         return [i for i in self.porta_halteres.values() if i != 0]
+    
+    def listar_espacos(self):
+        return [i for i, j in self.porta_halteres.items() if j == 0]
 
     def pegar_haltere(self, peso):
         halt_pos = list(self.porta_halteres.values()).index(peso)
@@ -25,6 +29,53 @@ class Academia:
         num_caos = [i for i, j in self.porta_halteres.items() if i != j]
         return len(num_caos) / len(self.porta_halteres)
 
+class Usuario:
+    def __init__(self, tipo, academia):
+        self.tipo = tipo # 1 - normal | 2 - bagunceiro
+        self.academia = academia
+        self.peso = 0
+
+    def iniciar_treino(self):
+        lista_peso = self.academia.listar_halteres()
+        self.peso = random.choice(lista_peso)
+        self.academia.pegar_haltere(self.peso)
+
+    def finalizar_treino(self):
+        espacos = self.academia.listar_espacos()
+
+        if self.tipo == 1:
+            if self.peso in espacos:
+                self.academia.devolver_halter(self.peso, self.peso)
+            else:
+                pos = random.choice(espacos)
+                self.academia.devolver_halter(pos, self.peso)
+        
+        if self.tipo == 2:
+            pos = random.choice(espacos)
+            self.academia.devolver_halter(pos, self.peso)
+        self.peso = 0
 
 
-self = Academia()
+
+academia = Academia()
+
+usuarios = [Usuario(1, academia) for i in range(10)]
+usuarios += [Usuario(2, academia) for i in range(1)]
+random.shuffle(usuarios)
+
+list_chaos = []
+
+for k in range(50):
+    for i in range(10):
+        random.shuffle(usuarios)
+        for user in usuarios:
+            user.iniciar_treino()
+        for user in usuarios:
+            user.finalizar_treino()
+    list_chaos += [academia.calcular_caos()]
+
+import seaborn as sns
+sns.displot(list_chaos)
+
+#academia.porta_halteres
+#academia.calcular_caos()
